1328a19f58a4f37658dc0270df9299df5068bcae
Chelsie L Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-01-09 11:27:39 -1000

    ongoing modifications to previous commit

--- dashboard.php
+++ dashboard.php
@@ -16,95 +16,95 @@
 
 <!-- this is a test.  test. -->
 <header class="sticky-top">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-sm-3 col-md-2 bg-lightsidebar">
-        <ul class="nav nav-sidebar">
-          <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link" href="nav-brand">
-                  <img src="./assets/images/logo-2.svg" height="250" alt="Cop or Not">
-                </a>
-              </li>
-              <li>
-                <a class="nav-link" href="./index.php">
-                  <img src="./assets/images/home.svg" alt="home">
-                  Home
-                </a>
-              </li>
-              <a class="nav-link" href="./login.php">
-                <img src="./assets/images/display.svg" alt="login">
-                Login
+<div class="container-fluid">
+  <div class="row">
+    <nav class="col-md-2 d-none d-xxl-block bg-light sidebar">
+      <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link" href="nav-brand">
+              <img src="./assets/images/logo-2.svg" height="250" alt="Cop or Not">
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="./index.php">
+              <img src="./assets/images/home.svg" alt="home">
+              Home
               </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./dashboard.php">
-                  <img src="./assets/images/activity.svg" alt="dashboard">
-                  Dashboard
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./swipe2.php">
-                  <img src="./assets/images/globe.svg" alt="marketplace">
-                  Swipe Marketplace
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./contact2.php">
-                  <img src="./assets/images/briefcase.svg" alt="contact">
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </div>
-          </nav>
-
-
-
-          <body>
-            <main role="main" class="col-sm-3 ml-sm-auto col-lg-10 pt-3 px-4">
-              <div class="d-flex align-items-center pb-2 mb-3 border-bottom">
-                <h1 class="text-muted">Dashboard</h1>
-              </div>
-
-              <div class="grid-container">
-                <div class="item level-2">
-                  <img src="https://publish.one37pm.net/wp-content/uploads/2021/02/punks.png?fit=600%2C600">
-                </div>
-                <div class="item level-3">
-                  <img src="https://publish.one37pm.net/wp-content/uploads/2021/02/EwKNWFRXIAA94V4.png?fit=600%2C600">
-                </div>
-                <div class="item level-4">
-                  <img src="https://lh3.googleusercontent.com/1odSUagF0-kjM8wJbY-oyNjyZWVvQWxmpRmFms92PWb_upzODfMOagpuv5rdl_5livW8svL5d3wgA0ciCCic8gpJu0xQCkDfMK6S=w600">
-                </div>
-                <div class="item level-5">
-                  <img src="https://www.larvalabs.com/cryptopunks/cryptopunk2460.png">
-                </div>
-                <div class="item level-1">
-                  <img src="https://www.reuters.com/resizer/m2Eze1n30GaqsQDKILr2Ukc9304=/631x0/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/7GBCHQUCEROJDPEVYQW7XG7VAE.jpg">
-                </div>
-                <div class="item level-2">
-                  <img src="https://lh3.googleusercontent.com/-2lJuLt70dQwg1OHy1TfKt3Wsc0zO6hSKHxsRlBDLIOsanPq9TF-e91oCrBivatUX-g3oFUFwf7FnhVIqTV3CB6fJcJ2mbdo1W5KuQ">
-                </div>
-                <div class="item level-3">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgZfngM3Hlhl1dEiDLa8g-V8Es242YzfwMIIL4BQE9FJmar_UrdFBzaGoTvscPVTavUFI&usqp=CAU">
-                </div>
-                <div class="item level-4">
-                  <img src="https://lh3.googleusercontent.com/zXcV_BJqVPf0IZZUrffjKD_cZpgFzYD8jsUYgZwGzhyhTF5hmQrOGMQQ7mEEjNUujRvtLeemeO2bpl4HL2uuoLrLMwpWmAZCgsU66A=w1400-k">
-                </div>
-              </div>
-            </main>
-          </body>
+          </li>
+          <a class="nav-link" href="./login.php">
+              <img src="./assets/images/display.svg" alt="login">
+              Login
+              </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./dashboard.php">
+              <img src="./assets/images/activity.svg" alt="dashboard">
+              Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./swipe2.php">
+              <img src="./assets/images/globe.svg" alt="marketplace">
+              Swipe Marketplace
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./contact2.php">
+              <img src="./assets/images/briefcase.svg" alt="contact">
+              Contact Us
+            </a>
+          </li>
+        </ul>
       </div>
-    </div>
+    </nav>
 
 
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="./assets/js/script.js"></script>
+
+    <body>
+      <main role="main" class="col-sm-3 ml-sm-auto col-lg-10 pt-3 px-4">
+        <div class="d-flex align-items-center pb-2 mb-3 border-bottom">
+          <h1 class="text-muted">Dashboard</h1>
+        </div>
+
+        <div class="grid-container">
+          <div class="item level-2">
+            <img src="https://publish.one37pm.net/wp-content/uploads/2021/02/punks.png?fit=600%2C600">
+          </div>
+          <div class="item level-3">
+            <img src="https://publish.one37pm.net/wp-content/uploads/2021/02/EwKNWFRXIAA94V4.png?fit=600%2C600">
+          </div>
+          <div class="item level-4">
+            <img src="https://lh3.googleusercontent.com/1odSUagF0-kjM8wJbY-oyNjyZWVvQWxmpRmFms92PWb_upzODfMOagpuv5rdl_5livW8svL5d3wgA0ciCCic8gpJu0xQCkDfMK6S=w600">
+          </div>
+          <div class="item level-5">
+            <img src="https://www.larvalabs.com/cryptopunks/cryptopunk2460.png">
+          </div>
+          <div class="item level-1">
+            <img src="https://www.reuters.com/resizer/m2Eze1n30GaqsQDKILr2Ukc9304=/631x0/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/7GBCHQUCEROJDPEVYQW7XG7VAE.jpg">
+          </div>
+          <div class="item level-2">
+            <img src="https://lh3.googleusercontent.com/-2lJuLt70dQwg1OHy1TfKt3Wsc0zO6hSKHxsRlBDLIOsanPq9TF-e91oCrBivatUX-g3oFUFwf7FnhVIqTV3CB6fJcJ2mbdo1W5KuQ">
+          </div>
+          <div class="item level-3">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgZfngM3Hlhl1dEiDLa8g-V8Es242YzfwMIIL4BQE9FJmar_UrdFBzaGoTvscPVTavUFI&usqp=CAU">
+          </div>
+          <div class="item level-4">
+            <img src="https://lh3.googleusercontent.com/zXcV_BJqVPf0IZZUrffjKD_cZpgFzYD8jsUYgZwGzhyhTF5hmQrOGMQQ7mEEjNUujRvtLeemeO2bpl4HL2uuoLrLMwpWmAZCgsU66A=w1400-k">
+          </div>
+        </div>
+      </main>
+    </body>
+  </div>
+</div>
+
+
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="./assets/js/script.js"></script>
 
 </header>
-
 </html>
